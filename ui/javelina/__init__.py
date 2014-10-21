4db6c58d0e1884b43c61a503a9873f45865432b7
Jeff Perry <jeffsp@gmail.com>
2014-10-21 16:51:53 -0500

    don't read settings from env

--- ui/javelina/__init__.py
+++ ui/javelina/__init__.py
@@ -13,7 +13,6 @@ from flask.ext.gravatar import Gravatar
 # create our application
 app = Flask(__name__)
 app.config.from_object('config')
-#app.config.from_envvar('JAVELINA_SETTINGS_FILE')
 
 from javelina import views
 from javelina import models
