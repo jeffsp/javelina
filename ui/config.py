8f6b0690d4dcf38511f84b9fee57be517a8b083b
Jeff Perry <jeffsp@gmail.com>
2014-10-21 16:51:13 -0500

    add variables for getting url root and upload paths

--- ui/config.py
+++ ui/config.py
@@ -1,3 +1,11 @@
+import os
+
 CSRF_ENABLED = True
 DEBUG=True
-UPLOAD_FOLDER='javelina/static/images'
+APP_ROOT=os.path.join(os.path.dirname(os.path.abspath(__file__)),'javelina')   # refers to application_top
+APP_STATIC=os.path.join(APP_ROOT, 'static')
+UPLOAD_FOLDER=os.path.join(APP_STATIC, 'uploads')
+RESIZED_FOLDER=os.path.join(UPLOAD_FOLDER, 'resized')
+RESIZED_URL='static/uploads/resized/'
+
+SECRET_KEY="super_duper_secret_key"
